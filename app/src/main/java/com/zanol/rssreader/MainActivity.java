7509d377b7a0971d3d1d80e78e073bff404c4e99
Willian Zanol <willianzanoll@hotmail.com>
2020-12-05 20:46:45 -0300

    Creted RSS downloader

---
+++ app/src/main/java/com/zanol/rssreader/MainActivity.java
@@ -0,0 +1,93 @@
+package com.zanol.rssreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: iniciando a AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        Log.d(TAG, "onCreate: terminou.");
+    }
+
+    //Parametros: envio, progresso, resultado
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: o parâmetro recebido é " + s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground: recebeu " + strings[0]);
+            String contents = downloadContents(strings[0]);
+
+            if(contents == null){
+                Log.e(TAG, "doInBackground: Erro baixando dados.");
+            }
+
+            return contents;
+        }
+
+        private String downloadContents(String urlPath){
+            StringBuilder result = new StringBuilder();
+
+            try{
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int responseCode = connection.getResponseCode();
+                Log.d(TAG, "downloadContents: O cóidigo de resposta foi: " + responseCode);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charsRead;
+                char[] inputBuffer = new char[500];
+
+                while(true){
+                    charsRead = reader.read(inputBuffer);
+                    if(charsRead < 0){
+                        break;
+                    }
+
+                    if(charsRead > 0){
+                        result.append(String.copyValueOf(inputBuffer, 0, charsRead));
+                    }
+                }
+
+                reader.close();
+
+                return result.toString();
+            }catch (MalformedURLException e){
+                Log.e(TAG, "downloadContents: URL inválida: " + e.getMessage());
+            }catch (IOException e) {
+                Log.e(TAG, "downloadContents: IOException ao ler os dados: " + e.getMessage());
+            }catch (SecurityException e){
+                Log.e(TAG, "downloadContents: Exceção de segurança. Falta permissão? " + e.getMessage());
+            }
+
+            return null;
+        }
+    }
+}
